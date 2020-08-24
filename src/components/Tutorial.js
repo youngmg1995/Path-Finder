2d247c37055dd83d5d809ce08424f19d07e14548
Mitchell Young <youngmg3000@gmail.com>
2020-08-24 16:23:12 -0400

    Fixed flex layout issue with tutorial pages

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -123,24 +123,6 @@ function Tutorial(props) {
                         <div className="Tutorial-Header-Spacer"></div>
                         <h1 className="Tutorial-Page-Header">Controls</h1>
                         <div className="Tutorial-Page-Content">
-                            <h3 className="Page-Sub-Header">What is a Path-Finding Algorithm?</h3>
-                            <p className="Page-Instructions-1">Path-finding algorithms are computer programs used to search for paths from one place to another.</p>
-                            <div className="Image-Wrapper-1"><img className="Image-1" alt="loading..." src={PathFinding}/></div>
-                            <p className="Page-Instructions-2">Path-finding algorithms come in two flavors <b>Weighted</b> and <b>Unweighted</b>. Unweighted algorithms simply account for the number of moves from one node to another, while Weighted algorithms also take into account additional costs associated with each move. </p>
-                            <div className="Content-Box-1">
-                                <div className="Image-Wrapper-2"><b>Weighted</b><img className="Image-2" alt="loading..." src={Weighted}/></div>
-                                <div className="Image-Wrapper-3"><b>Unweighted</b><img className="Image-3" alt="loading..." src={Unweighted}/></div>
-                            </div>
-                            <p className="Page-Instructions-3">Some algorithms guarantee the shortest path possible will always be found, while others do not.</p>
-                            <div className="Content-Box-2">
-                                <div className="Image-Wrapper-4"><b>Shortest Path</b><img className="Image-4" alt="loading..." src={Shortest}/></div>
-                                <div className="Image-Wrapper-5"><b>Not Shortest Path</b><img className="Image-5" alt="loading..." src={NotShortest}/></div>
-                            </div>
-                            <p className="Page-Instructions-4">To be more efficient, some algorithms even utilitze a distance heuristic to incentivise the path to move closer to the target rather than away from it.</p>
-                            <div className="Content-Box-3">
-                                <div className="Image-Wrapper-6"><b>Uses Heuristic</b><img className="Image-6" alt="loading..." src={Heuristic}/></div>
-                                <div className="Image-Wrapper-7"><b>No Heuristic</b><img className="Image-7" alt="loading..." src={NoHeuristic}/></div>
-                            </div>
                         </div>
                     </div>
                 }
